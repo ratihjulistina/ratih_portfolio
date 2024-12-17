47b1eaa235b8254b4bf27ef1b89ad302e2fd5947
ratihjulistina <42568042+ratihjulistina@users.noreply.github.com>
2024-12-17 11:12:23 +0700

    Update global.customhooks.tsx

--- src/hooks/global.customhooks.tsx
+++ src/hooks/global.customhooks.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export function useGlobal(value: boolean) {
+export function useGlobal() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
